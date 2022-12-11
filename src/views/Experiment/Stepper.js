256131b382ac15e01c3ebcfaa9c20097343d151c
Umeh Amaka <amakaumeh01@gmail.com>
2022-12-11 23:30:03 +0100

    correct stepper color

--- src/views/Experiment/Stepper.js
+++ src/views/Experiment/Stepper.js
@@ -36,10 +36,45 @@ export default function CustomStepper({activeStep, setActiveStep}) {
 
   return (
     <Box sx={{ maxWidth: 400 }}>
-      <Stepper activeStep={activeStep} orientation="vertical">
+      <Stepper activeStep={activeStep} orientation="vertical"
+        sx={{
+          '.MuiMobileStepper-dot': { backgroundColor: '#CCCCCC' },
+          '.MuiMobileStepper-dotActive': { backgroundColor: '#666666' },
+      }}
+      >
         {steps.map((step, index) => (
-          <Step key={step.label}>
-            <StepLabel>
+          <Step key={step.label}
+            sx={{
+              '& .MuiStepLabel-root .Mui-completed': {
+                color: '#817FDD !important', // circle color (COMPLETED)
+                
+              },
+              '& .MuiStepLabel-label.Mui-completed.MuiStepLabel-alternativeLabel':
+                {
+                  color: '#817FDD !important', // Just text label (COMPLETED)
+                  fontWeight: 'bold !important',
+
+                },
+              '& .MuiStepLabel-root .Mui-active': {
+                color: '#403dd1 !important', // circle color (ACTIVE)
+              },
+              '& .MuiStepLabel-label.Mui-active.MuiStepLabel-alternativeLabel':
+                {
+                  color: '#817FDD !important', // Just text label (ACTIVE)
+                  fontWeight: 'bold !important',
+                },
+              '& .MuiStepLabel-root .Mui-active .MuiStepIcon-text': {
+                fill: 'black', // circle's number (ACTIVE)
+              },
+              '& .css-o68c8a-MuiStepLabel-label': {
+                color: '#515152', // circle's number (ACTIVE)
+              },
+              // '& .css-o68c8a-MuiStepLabel-label': {
+              //   color: 'red', // circle's number (ACTIVE)
+              // },
+            }}
+          >
+            <StepLabel sx={{color: 'red'}}>
               {step.label}
             </StepLabel>
           </Step>
